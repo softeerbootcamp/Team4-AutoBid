2cbf0998beb766dd1c8d1b8faf70b474e30c5bf4
wonju-dev <wonjulee.dev@gmail.com>
2023-02-02 12:03:41 +0900

    feat: Car domain 필드 추가, 빌더 생성

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Car.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Car.java
@@ -1,5 +1,6 @@
 package com.codesquad.autobid.car.domain;
 
+import com.codesquad.autobid.handler.car.vo.CarVO;
 import com.codesquad.autobid.user.User;
 
 import java.time.LocalDateTime;
@@ -8,14 +9,28 @@ public class Car {
 
     private Long id;
     private User user;
-    // todo: state의 역할?
-    private String state;
-    private Long distance;
-    private String name;
-    private String sellName;
-    // todo: carUid 역할?
-    private Long carUid;
+    private State state;
     private Category category;
+    private Long distance;
+    // 현대차 API에서 사용하는 차량 식별용 아이디
+    private String carId;
+    private String carNickname;
+    private String carType;
+    private String carName;
+    private String carSellname;
     private LocalDateTime createdAt;
     private LocalDateTime updatedAt;
+
+    private Car() {
+    }
+
+    public static Car from(CarVO carVO) {
+        Car car = new Car();
+        car.carId = carVO.getCarId();
+        car.carNickname = carVO.getCarNickname();
+        car.carType = carVO.getCarType();
+        car.carName = carVO.getCarName();
+        car.carSellname = carVO.getCarSellname();
+        return car;
+    }
 }
