06ca45c52389d3d82eba124428203bb5ed4003a7
wonju-dev <wonjulee.dev@gmail.com>
2023-02-02 12:08:30 +0900

    feat: 차량 조회 API 전용 VO 구성
    CarFindErrorResponseVO는 조회 실패시 발생하는 JSON을 객체와 매핑하기 위한 클래스이다.
    CarVO는 차량 정보 JSON을 객체와 매핑하기 위한 클래스이다.

---
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/handler/car/vo/CarFindErrorResponseVO.java
@@ -0,0 +1,19 @@
+package com.codesquad.autobid.handler.car.vo;
+
+public class CarFindErrorResponseVO {
+    private String errId;
+    private int errCode;
+    private String errMsg;
+
+    public String getErrId() {
+        return errId;
+    }
+
+    public int getErrCode() {
+        return errCode;
+    }
+
+    public String getErrMsg() {
+        return errMsg;
+    }
+}
