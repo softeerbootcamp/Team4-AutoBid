e1fba194ef03dbf38686ea65a2e9d6773607a3e6
RandomlyChristen <yylwd2000@naver.com>
2023-02-04 19:14:29 +0900

    feat: #38 Component 최상위 클래스 구현
    stateSelector: 전역 state 에서 이 컴포넌트가 관측할 nasted state 레퍼런스 접근방법 정의
    addEvent: 이벤트 델리게이션 패턴
    onStateChanged: store 상태변화에 따른 행동 정의
    
    생애주기: initialize -> render{ template -> mounted } -> onStateChanged

---
+++ AutoBid_FE/src/core/component.ts
@@ -0,0 +1,62 @@
+import GlobalStore from "./store";
+import {Unsubscribe} from "redux";
+
+class Component<State, Props = {}> {
+    $target: HTMLElement;
+    props: Props;
+    state: State|undefined;
+    #unsubscribe: Unsubscribe|undefined;
+
+    constructor($target: HTMLElement, props: Props) {
+        this.$target = $target;
+        this.props = props;
+        this.#initState();
+        this.initialize();
+        this.render();
+    }
+
+    initialize(): void {}
+    onStateChanged(newLocalState: State): void {}
+    stateSelector(globalState: any): State|undefined { return undefined; }
+    mounted() {}
+    template(): InnerHTML['innerHTML'] { return ''; }
+
+    addEvent(type: string, selectors: string, listener: EventListener) {
+        const children = [ ...this.$target.querySelectorAll(selectors) ];
+        const isTarget = ({ target }: Event) => {
+            const targetElement = target as Element;
+            return children.includes(targetElement) || (targetElement).closest(selectors);
+        }
+        this.$target.addEventListener(type, e => {
+            if (!isTarget(e)) return false;
+            return listener(e);
+        });
+    }
+
+    #initState() {
+        const store = GlobalStore.get();
+        const globalState = store.getState();
+        const localState = this.stateSelector(globalState);
+        if (!localState) return;
+        this.state = localState;
+        this.#unsubscribe = store.subscribe(() => {
+            if (!this.$target.isConnected && this.#unsubscribe) {
+                this.#unsubscribe();
+                return;
+            }
+            const globalState = store.getState();
+            const newLocalState = this.stateSelector(globalState);
+            if (newLocalState && this.state !== newLocalState) {
+                this.onStateChanged(newLocalState);
+                this.state = newLocalState;
+            }
+        });
+    }
+
+    render() {
+        this.$target.innerHTML = this.template();
+        this.mounted();
+    }
+}
+
+export default Component;
