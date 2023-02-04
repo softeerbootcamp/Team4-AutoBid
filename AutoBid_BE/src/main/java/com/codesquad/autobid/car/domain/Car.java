67391cf4d5de5eeedcd859d0281bea2957800fa2
roopre <benny1020@naver.com>
2023-02-04 17:11:36 +0900

    fix: 빌드 안되는 오류 해결 User 클래스 삭제 Car 클래스에서 Users 클래스를 import 하도록 수정

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Car.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Car.java
@@ -1,13 +1,13 @@
 package com.codesquad.autobid.car.domain;
 
 import com.codesquad.autobid.handler.car.vo.CarVO;
-import com.codesquad.autobid.user.domain.User;
+import com.codesquad.autobid.user.domain.Users;
+
 import org.springframework.data.annotation.AccessType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Table;
 
-import java.sql.ResultSet;
 import java.time.LocalDateTime;
 
 @Table("car")
@@ -18,7 +18,7 @@ public class Car {
     private Long id;
     @Column("user_id")
     @AccessType(AccessType.Type.PROPERTY)
-    private User user;
+    private Users user;
 
     // @Column("car_state")
     // @AccessType(AccessType.Type.PROPERTY)
