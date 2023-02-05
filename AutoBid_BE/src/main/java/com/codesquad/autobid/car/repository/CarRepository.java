2186a83e91e21e511ea66b1b323fef6c1e001836
wonju-dev <wonjulee.dev@gmail.com>
2023-02-05 23:49:11 +0900

    feat(CarRowMapper): CarRowMapper 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/repository/CarRepository.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/repository/CarRepository.java
@@ -1,6 +1,7 @@
 package com.codesquad.autobid.car.repository;
 
 import com.codesquad.autobid.car.domain.Car;
+import com.codesquad.autobid.car.repository.rowMapper.CarRowMapper;
 import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -8,6 +9,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface CarRepository extends CrudRepository<Car, Long> {
-    @Query(value = "SELECT * FROM car WHERE user_id = :id")
+    @Query(value = "SELECT * FROM car WHERE user_id = :id", rowMapperClass = CarRowMapper.class)
     List<Car> findCarsByUserId(@Param("id") Long id);
 }
