3880ef51bcce7eb37304ddbe35d488a7dd35f324
wonju-dev <wonjulee.dev@gmail.com>
2023-02-05 23:51:55 +0900

    feat(CarController): 차량 조회 API 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/controller/CarController.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/controller/CarController.java
@@ -2,36 +2,32 @@ package com.codesquad.autobid.car.controller;
 
 import com.codesquad.autobid.car.domain.Car;
 import com.codesquad.autobid.car.service.CarService;
-import com.codesquad.autobid.handler.car.CarHandler;
+import com.codesquad.autobid.user.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
 public class CarController {
 
     private final Logger logger = LoggerFactory.getLogger(CarController.class);
-    private final CarHandler carHandler;
     private final CarService carService;
 
     @Autowired
-    public CarController(CarHandler carHandler, CarService carService) {
-        this.carHandler = carHandler;
+    public CarController(CarService carService) {
         this.carService = carService;
     }
 
     @GetMapping("/user/{id}/cars")
-    public List<Car> getCars(@PathVariable("id") Long id, @RequestParam(name = "refresh", required = false) boolean refresh, String accessToken) {
-        logger.info("id={}, accessToken={}", id, accessToken);
-        if (refresh) {
-            return carHandler.getCars(accessToken);
-        }
-        return carService.getCars(id);
+    // todo: filter(interceptor)에서 session 받는걸로 변경하기
+    // @SessionAttribute(name = "user") User user, @SessionAttribute(name = "accessToken") String accessToken, boolean refresh
+    public List<Car> getCars(@PathVariable("id") Long id, boolean refresh, String accessToken) {
+        logger.info("user/{}/cars refresh={} accessToken={}", id,refresh, accessToken);
+        return carService.getCars(id, accessToken, refresh);
     }
 }
