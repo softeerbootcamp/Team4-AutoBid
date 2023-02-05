1f2eb5f0da1721cafaaabea63d17ab2d4a9e2d8b
wonju-dev <wonjulee.dev@gmail.com>
2023-02-05 23:45:21 +0900

    feat(Type): 필드 추가

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Type.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Type.java
@@ -1,21 +1,16 @@
 package com.codesquad.autobid.car.domain;
 
-import java.util.Optional;
-
 public enum Type {
     GN("내연기관"),
     EV("전기"),
     HEV("하이브리드"),
     PHEV("플러그인하이브리드"),
-    FCEV("수소전기");
+    FCEV("수소전기"),
+    ETC("그 외");
 
     private String description;
 
     Type(String description) {
         this.description = description;
-    }
-
-    public static Type findByDescription(String description) {
-        return Type.valueOf(description);
     }
 }
