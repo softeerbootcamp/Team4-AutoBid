dea85682a50eeedab5eed8be8a05c0b781196b84
wonju-dev <wonjulee.dev@gmail.com>
2023-02-07 00:25:12 +0900

    remove: 미사용 import 제거

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Car.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/Car.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.convert.ValueConverter;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.relational.core.mapping.Column;
 import org.springframework.data.relational.core.mapping.Embedded;
