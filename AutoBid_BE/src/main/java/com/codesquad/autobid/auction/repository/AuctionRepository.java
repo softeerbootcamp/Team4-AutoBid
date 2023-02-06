16868202db72b8343747a5a431c04dfe773394c7
roopre <benny1020@naver.com>
2023-02-07 00:02:48 +0900

    feat: 경매 테이블 접근 클래스인 AuctionRepository 구현

---
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/repository/AuctionRepository.java
@@ -0,0 +1,9 @@
+package com.codesquad.autobid.auction.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codesquad.autobid.auction.domain.Auction;
+
+public interface AuctionRepository extends CrudRepository<Auction, Long> {
+
+}
