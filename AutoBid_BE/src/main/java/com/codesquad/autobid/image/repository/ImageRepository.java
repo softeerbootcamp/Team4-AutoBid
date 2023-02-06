d628fd371729be674dddc1c14e1c9b20d8255b54
roopre <benny1020@naver.com>
2023-02-07 00:03:39 +0900

    feat: image 테이블에 접근할 ImageRepository 구현

---
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/image/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.codesquad.autobid.image.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.codesquad.autobid.image.domain.Image;
+
+public interface ImageRepository extends CrudRepository<Image, Long> {
+
+}
