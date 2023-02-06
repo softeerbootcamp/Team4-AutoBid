e39d79bb0572fffb3c1c44eb7afff4f151b1d2a7
wonju-dev <wonjulee.dev@gmail.com>
2023-02-06 09:56:17 +0900

    remove: 미사용 import 제거

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/controller/CarController.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/controller/CarController.java
@@ -2,14 +2,13 @@ package com.codesquad.autobid.car.controller;
 
 import com.codesquad.autobid.car.domain.Car;
 import com.codesquad.autobid.car.service.CarService;
-import com.codesquad.autobid.user.domain.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
