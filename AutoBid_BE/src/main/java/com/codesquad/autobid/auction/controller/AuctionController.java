bc22e2a87d2c5bde5dfd03390a24c2a6b7186d83
roopre <benny1020@naver.com>
2023-02-07 11:13:49 +0900

    feat: 경매 생성 API 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/controller/AuctionController.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/controller/AuctionController.java
@@ -2,10 +2,12 @@ package com.codesquad.autobid.auction.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.SessionAttribute;
 
@@ -13,8 +15,12 @@ import com.codesquad.autobid.auction.request.AuctionRegisterRequest;
 import com.codesquad.autobid.auction.service.AuctionService;
 import com.codesquad.autobid.user.domain.User;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 
+@Tag(name = "Auction", description = "경매 API")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auction")
@@ -22,7 +28,8 @@ public class AuctionController {
 
 	public final AuctionService auctionService;
 
-	@PostMapping()
+	@Operation(summary = "경매 생성 API", description = "경매를 등록한다.")
+	@PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
 	public void auctionAdd(@ModelAttribute AuctionRegisterRequest auctionRegisterRequest, @SessionAttribute(value = "user") User user) {
 		auctionService.addAuction(auctionRegisterRequest, user);
 	}
