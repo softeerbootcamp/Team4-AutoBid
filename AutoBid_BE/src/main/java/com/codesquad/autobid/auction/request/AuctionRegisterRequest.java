0185a2a24b1af0a9133e33aa19b56926f27f7e17
roopre <benny1020@naver.com>
2023-02-07 13:26:34 +0900

    fix: AuctionRegisterRequest에서 LocalDateTime 바인딩 안되는 에러 수정

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/request/AuctionRegisterRequest.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/request/AuctionRegisterRequest.java
@@ -10,19 +10,23 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
+@ToString
 @Getter
+@Setter
 public class AuctionRegisterRequest {
 	@Schema(description = "")
 	private List<MultipartFile> multipartFileList;
 	@Schema
 	private Long carId;
 	@Schema
-	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime auctionStartTime;
 	@Schema
-	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME, pattern = "yyyy-MM-dd HH:mm")
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime auctionEndTime;
 	@Schema
