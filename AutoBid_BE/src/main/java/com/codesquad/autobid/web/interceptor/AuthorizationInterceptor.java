b26cc32aec651ef4a983e9a36d695fcae635a9f9
roopre <benny1020@naver.com>
2023-02-07 13:45:01 +0900

    feat: 특정 헤더 들어오면 테스트 유저를 세션에 넣어주도록 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/web/interceptor/AuthorizationInterceptor.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/web/interceptor/AuthorizationInterceptor.java
@@ -1,6 +1,8 @@
 package com.codesquad.autobid.web.interceptor;
 
 import com.codesquad.autobid.OauthToken;
+import com.codesquad.autobid.test.UserTestUtil;
+
 import org.springframework.web.servlet.HandlerInterceptor;
 
 import javax.servlet.http.HttpServletRequest;
@@ -8,13 +10,20 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 public class AuthorizationInterceptor implements HandlerInterceptor {
-    @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
-        HttpSession session = request.getSession(false);
-        if (session == null || session.getAttribute(OauthToken.ACCESS_TOKEN_KEY) == null) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
-        }
-        return true;
-    }
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+		if (request.getHeader("Authorization") != null) {
+			HttpSession session = request.getSession();
+			session.setAttribute("user", UserTestUtil.TEST_USER_1);
+			session.setAttribute("accessToken", "accessToken");
+		}
+
+		HttpSession session = request.getSession(false);
+		if (session == null || session.getAttribute(OauthToken.ACCESS_TOKEN_KEY) == null) {
+			response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+			return false;
+		}
+
+		return true;
+	}
 }
