3fac48f1ce2d17bdbda52a0d60ffc982483b39ae
roopre <benny1020@naver.com>
2023-02-07 11:14:14 +0900

    feat: 경매 생성 Request 클래스 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/request/AuctionRegisterRequest.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/request/AuctionRegisterRequest.java
@@ -8,19 +8,24 @@ import org.springframework.web.multipart.MultipartFile;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
 
 @Getter
 public class AuctionRegisterRequest {
+	@Schema(description = "")
 	private List<MultipartFile> multipartFileList;
+	@Schema
 	private Long carId;
+	@Schema
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime auctionStartTime;
+	@Schema
 	@DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm")
 	private LocalDateTime auctionEndTime;
+	@Schema
 	private Long auctionStartPrice;
-
 
 }
