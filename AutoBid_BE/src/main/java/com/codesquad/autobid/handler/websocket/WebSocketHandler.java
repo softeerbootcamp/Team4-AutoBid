70dd21ac7196113ceb2dfc00593bc323b9fddf4e
tjdwns4537 <tjdwns4537@naver.com>
2023-02-08 18:27:47 +0900

    feat: websocket config 추가

---
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/handler/websocket/WebSocketHandler.java
@@ -0,0 +1,28 @@
+package com.codesquad.autobid.handler.websocket;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+@Component
+public class WebSocketHandler extends TextWebSocketHandler {
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        String msg = message.getPayload();
+        TextMessage textMessage = new TextMessage("your message is " + msg);
+        session.sendMessage(textMessage);
+    }
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+}
