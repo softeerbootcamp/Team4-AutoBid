1b27c7cf71bf00d0e976327f349837c8df628b96
WonjuLee <wonjulee.dev@gmail.com>
2023-02-08 14:49:22 +0900

    feat: Spring Scheduler

--- AutoBid_BE/src/main/java/com/codesquad/autobid/AutoBidApplication.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/AutoBidApplication.java
@@ -3,15 +3,19 @@ package com.codesquad.autobid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jdbc.repository.config.EnableJdbcAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 @EnableJdbcAuditing
 @SpringBootApplication
+@EnableScheduling
+@EnableAsync
 public class AutoBidApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AutoBidApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AutoBidApplication.class, args);
+    }
 
 }
