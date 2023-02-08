d8450d45ca5cb14bdbbb381e8c7007b46d6dfe20
WonjuLee <wonjulee.dev@gmail.com>
2023-02-08 14:59:28 +0900

    feat: lockProvider Bean 등록

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/SchedulerConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/SchedulerConfig.java
@@ -1,11 +1,15 @@
 package com.codesquad.autobid.config;
 
+import net.javacrumbs.shedlock.core.LockProvider;
+import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.SchedulingConfigurer;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
 import org.springframework.scheduling.config.ScheduledTaskRegistrar;
 
+import javax.sql.DataSource;
 import java.util.concurrent.Executor;
 
 @Configuration
@@ -27,5 +31,10 @@ public class SchedulerConfig implements AsyncConfigurer, SchedulingConfigurer {
     @Override
     public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
         taskRegistrar.setTaskScheduler(this.threadPoolTaskScheduler());
+    }
+
+    @Bean
+    public LockProvider lockProvider(DataSource dataSource) {
+        return new JdbcTemplateLockProvider(dataSource);
     }
 }
