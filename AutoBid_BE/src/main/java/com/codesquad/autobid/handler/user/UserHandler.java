a6ee688496b520d9d2af65e7959567f580fbf120
tjdwns4537 <tjdwns4537@naver.com>
2023-02-08 14:59:26 +0900

    Test: 리프레시 토큰으로 재발급 테스트 코드

--- AutoBid_BE/src/main/java/com/codesquad/autobid/handler/user/UserHandler.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/handler/user/UserHandler.java
@@ -3,8 +3,11 @@ package com.codesquad.autobid.handler.user;
 import com.codesquad.autobid.OauthToken;
 import com.codesquad.autobid.user.domain.UserVO;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -12,16 +15,18 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-@Service
+@Slf4j
+@Component
 public class UserHandler {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final RestTemplate rt = new RestTemplate();
 
     @Value("${hyundai.auth.token_profile_url}")
     private String apiURL;
 
     public UserVO userProfileAPICall(OauthToken oauthToken) {    // 발급받은 Access Token
         String access_token = oauthToken.getAccessToken();
-
-        ObjectMapper mapper = new ObjectMapper();
         UserVO user = new UserVO();
         StringBuffer sb;
         String responseData = "";
