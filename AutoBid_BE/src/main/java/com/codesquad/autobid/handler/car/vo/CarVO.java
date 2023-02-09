d7fb7539fc3669d34c5aa1c411421d8d674fcddc
WonjuLee <wonjulee.dev@gmail.com>
2023-02-09 15:12:27 +0900

    fix: add CarVO

--- AutoBid_BE/src/main/java/com/codesquad/autobid/handler/car/vo/CarVO.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/handler/car/vo/CarVO.java
@@ -1,26 +1,33 @@
 package com.codesquad.autobid.handler.car.vo;
 
 import com.codesquad.autobid.car.domain.Type;
+import com.codesquad.autobid.handler.car.enums.DistanceUnit;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
+@ToString
+@AllArgsConstructor
 public class CarVO {
 
+    @JsonProperty("carId")
     private String carId;
+    @JsonProperty("carType")
     private Type type;
+    @JsonProperty("carName")
     private String name;
+    @JsonProperty("carSellname")
     private String sellName;
-    private AvailableDistanceVO availableDistanceVO;
+    @JsonProperty("carNickname")
+    private String nickname;
+    @JsonIgnore
+    private String distance;
 
-    private CarVO() {
-    }
-
-    public AvailableDistanceVO getAvailableDistanceVO() {
-        return availableDistanceVO;
-    }
-
-    public CarVO addDistanceInfo(AvailableDistanceVO availableDistanceVO) {
-        this.availableDistanceVO = availableDistanceVO;
+    public CarVO setDistanceVO(DistanceVO distanceVO) {
+        this.distance = distanceVO.getDistance() + DistanceUnit.findByCode(distanceVO.getUnitCode()).getPlaceholder();
         return this;
     }
 }
