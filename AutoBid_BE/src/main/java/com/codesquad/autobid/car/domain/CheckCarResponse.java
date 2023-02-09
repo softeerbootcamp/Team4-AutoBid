31cd1c12adaf6befdd3829609ac148b51787b36d
WonjuLee <wonjulee.dev@gmail.com>
2023-02-09 16:10:08 +0900

    refactor: Response 타입 변경

--- AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/CheckCarResponse.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/car/domain/CheckCarResponse.java
@@ -8,7 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class CheckCarResponse {
 
-    private String distance;
+    private Long distance;
     private String name;
     private Type type;
     private String sellName;
@@ -16,7 +16,7 @@ public class CheckCarResponse {
 
     public static CheckCarResponse from(Car car) {
         CheckCarResponse response = new CheckCarResponse();
-        response.distance = car.getDistance().toString();
+        response.distance = car.getDistance();
         response.name = car.getName();
         response.type = car.getType();
         response.sellName = car.getSellName();
