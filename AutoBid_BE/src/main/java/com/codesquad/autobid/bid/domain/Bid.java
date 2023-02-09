44cd716ee084d07ec09816ec018672d24b708a25
roopre <benny1020@naver.com>
2023-02-09 18:06:32 +0900

    feat: 입찰 엔티티 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/bid/domain/Bid.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/bid/domain/Bid.java
@@ -3,6 +3,7 @@ package com.codesquad.autobid.bid.domain;
 import java.time.LocalDateTime;
 
 import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.data.relational.core.mapping.Column;
@@ -13,18 +14,23 @@ import com.codesquad.autobid.user.domain.User;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.ToString;
 
 @Getter
 @AllArgsConstructor
+@ToString
 @Table("bid")
 public class Bid {
 
+	@Id
 	@Column("bid_id")
 	private Long id;
 	@Column("auction_id")
 	private AggregateReference<Auction, Long> auctionId;
 	@Column("user_id")
 	private AggregateReference<User, Long> userId;
+	@Column("bid_price")
+	private Long price;
 	@Column("bid_accept")
 	private Boolean bidAccept;
 	@CreatedDate
@@ -34,15 +40,16 @@ public class Bid {
 	@Column("updated_at")
 	private LocalDateTime updatedAt;
 
-	private Bid(AggregateReference<Auction, Long> auctionId, AggregateReference<User, Long> userId, Boolean bidAccept) {
+	private Bid(AggregateReference<Auction, Long> auctionId, AggregateReference<User, Long> userId, Long price,
+		Boolean bidAccept) {
 		this.auctionId = auctionId;
 		this.userId = userId;
+		this.price = price;
 		this.bidAccept = bidAccept;
 	}
 
-	public static Bid of(AggregateReference<Auction, Long> auctionId, AggregateReference<User, Long> userId,
+	public static Bid of(AggregateReference<Auction, Long> auctionId, AggregateReference<User, Long> userId, Long price,
 		Boolean bidAccept) {
-		return new Bid(auctionId, userId, bidAccept);
+		return new Bid(auctionId, userId, price, bidAccept);
 	}
-
 }
