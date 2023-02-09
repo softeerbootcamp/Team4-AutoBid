c299997cf0041a3780bf870bd3974fb84bf19a49
tjdwns4537 <tjdwns4537@naver.com>
2023-02-09 19:39:33 +0900

    feat: 다수 소켓 열리게 코드 수정

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
@@ -1,24 +1,39 @@
 package com.codesquad.autobid.config;
 
+import com.codesquad.autobid.auction.service.AuctionService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.relational.core.sql.In;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     /**
-     *  TODO
-     *   1. 배치에서 누가 방만들지에 대한
-     * **/
+     * TODO
+     *   1. 방이 만들어질 리스트를 가져온다.
+     *   2. 해당 방들에 대해 웹 소켓을 연결해준다.
+     **/
 
-
+    List<Integer> testaAuctionRoomId = new ArrayList<>();
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/auction-room/").setAllowedOriginPatterns("http://localhost:8080","http://localhost:5500","http://localhost:3000")
-                .withSockJS();
+        testaAuctionRoomId.add(1);
+        testaAuctionRoomId.add(2);
+        testaAuctionRoomId.add(3);
+        testaAuctionRoomId.add(4);
+        for (Integer i : testaAuctionRoomId) {
+            registry.addEndpoint("/auction-room/"+i+"/").setAllowedOriginPatterns("http://localhost:8080","http://localhost:5500","http://localhost:3000")
+                    .withSockJS();
+        }
+//        registry.addEndpoint("/auction-room/").setAllowedOriginPatterns("http://localhost:8080","http://localhost:5500","http://localhost:3000")
+//                .withSockJS();
     }
 
     @Override
