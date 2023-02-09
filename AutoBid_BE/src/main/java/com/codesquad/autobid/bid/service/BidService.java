ac60215a824fa737aa9f7ce61002977f7298ea70
roopre <benny1020@naver.com>
2023-02-09 18:07:26 +0900

    feat: 입찰하는 로직 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/bid/service/BidService.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/bid/service/BidService.java
@@ -1,5 +1,7 @@
 package com.codesquad.autobid.bid.service;
 
+import javax.swing.text.StyledEditorKit;
+
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +18,21 @@ public class BidService {
 
 	private final BidRepository bidRepository;
 
-	public void addBid(BidRegisterRequest bidRegisterRequest, User user) {
-		//Bid bid = Bid.of(AggregateReference.to(bidRegisterRequest.getAuctionId()), user.getId(), )
-		//bidRepository.save()
+	public Boolean suggestBid(BidRegisterRequest bidRegisterRequest, User user) {
+		if (!checkBid(bidRegisterRequest.getSuggestedPrice(), bidRegisterRequest.getSuggestedPrice(), user)) {
+			return false;
+		}
+		Bid bid = Bid.of(AggregateReference.to(bidRegisterRequest.getAuctionId()), AggregateReference.to(user.getId()),
+			bidRegisterRequest.getSuggestedPrice(), false);
+		bidRepository.save(bid);
+
+		return true;
+	}
+
+	// 입찰 가능한지 확인
+	// 동시성 해결 예정
+	public Boolean checkBid(Long price, Long auctionId, User User) {
+		return true;
 	}
 }
+
