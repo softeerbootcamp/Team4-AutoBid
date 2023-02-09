d44a9ef81edd526e67ef3e5e5cb1f10c3ce4fa24
tjdwns4537 <tjdwns4537@naver.com>
2023-02-09 17:43:05 +0900

    feat: 웹소켓 입장유저에 대한 기능

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
@@ -1,21 +1,29 @@
 package com.codesquad.autobid.config;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-//    @Override
-//    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(new WebSocketHandler(), "ws/chat").setAllowedOrigins("*");
-//    }
+    /**
+     *  TODO
+     *   1. 배치에서 누가 방만들지에 대한
+     * **/
+
+
 
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-//        registry.addHandler(new MyHandler(), "/myHandler").setAllowedOrigins("*").withSockJS();
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/auction-room/").setAllowedOriginPatterns("http://localhost:8080","http://localhost:5500","http://localhost:3000")
+                .withSockJS();
+    }
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/publish");
+        registry.enableSimpleBroker("/subscribe");
     }
 }
