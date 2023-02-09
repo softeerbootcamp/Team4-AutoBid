12cdc614e8d5c002da9a941519c69a3f205283a6
roopre <benny1020@naver.com>
2023-02-09 13:36:09 +0900

    feat: post test api 생성

--- AutoBid_BE/src/main/java/com/codesquad/autobid/test/TestController.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/test/TestController.java
@@ -7,13 +7,22 @@ import org.apache.http.HttpEntity;
 import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
 	@GetMapping("/test")
-	public String test(HttpServletRequest httpServletRequest) {
+	public String GetTest(HttpServletRequest httpServletRequest) {
+		HttpSession httpSession = httpServletRequest.getSession();
+		httpSession.setAttribute("test", "test");
+
+		return "OK";
+	}
+	
+	@PostMapping("/test")
+	public String postTest(HttpServletRequest httpServletRequest) {
 		HttpSession httpSession = httpServletRequest.getSession();
 		httpSession.setAttribute("test", "test");
 
