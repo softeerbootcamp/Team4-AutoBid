673936962f1f425c3385b647d1e13aac4179a83a
roopre <benny1020@naver.com>
2023-02-10 15:00:40 +0900

    feat: 입찰하기 API 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/bid/controller/BidController.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/bid/controller/BidController.java
@@ -1,4 +1,32 @@
 package com.codesquad.autobid.bid.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.codesquad.autobid.bid.request.BidRegisterRequest;
+import com.codesquad.autobid.bid.service.BidService;
+import com.codesquad.autobid.user.domain.User;
+import com.codesquad.autobid.web.argumentresolver.AuthorizedUser;
+
+import io.swagger.v3.oas.annotations.Parameter;
+
+@RestController
 public class BidController {
+
+	private final BidService bidService;
+
+	@Autowired
+	public BidController(BidService bidService) {
+		this.bidService = bidService;
+	}
+
+	@PostMapping("/auction/bid")
+	public ResponseEntity<Boolean> bidRegister(@Parameter BidRegisterRequest bidRegisterRequest, @Parameter(hidden = true) @AuthorizedUser User user) {
+		boolean result = bidService.suggestBid(bidRegisterRequest, user);
+
+		return ResponseEntity.status(HttpStatus.OK).body(result);
+	}
 }
