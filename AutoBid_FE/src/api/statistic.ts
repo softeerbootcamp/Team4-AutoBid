e125ea40b82c1dadecf817b6fbf198b76946456f
RandomlyChristen <yylwd2000@naver.com>
2023-02-12 02:48:09 +0900

    feat: api 테스팅 용 시간 지연 함수
    util.ts: lazyReturn -> setTimeout을 이용해 지정한 시간 후에 반환하는 프로미스 생성 함수

--- AutoBid_FE/src/api/statistic.ts
+++ AutoBid_FE/src/api/statistic.ts
@@ -1,31 +1,32 @@
 import {AuctionStatisticDTO} from "../model/statistic";
+import {asyncTaskWrapper, lazyReturn} from "../core/util";
 
-export const requestAuctionStatistic = async (): Promise<AuctionStatisticDTO> => {
-    return new Promise(res => {
-        setTimeout(() => res({
-            auctionStatistic: {
-                nSold: 114,
-                minPrice: 0,
-                maxPrice: 11200,
-                histogram: {
-                    binSize: 100,
-                    contents: [
-                        10,
-                        22,
-                        24,
-                        15,
-                        35,
-                        55,
-                        67,
-                        58,
-                        43,
-                        21,
-                        13,
-                        11,
-                        9
-                    ]
-                }
-            }
-        }), 1000);
-    });
-};
+const lazyStatistic = lazyReturn({
+    auctionStatistic: {
+        nSold: 114,
+        minPrice: 0,
+        maxPrice: 11200,
+        histogram: {
+            binSize: 100,
+            contents: [
+                10,
+                22,
+                24,
+                15,
+                35,
+                55,
+                67,
+                58,
+                43,
+                21,
+                13,
+                11,
+                9
+            ]
+        }
+    }
+} as AuctionStatisticDTO, 1000);
+
+export const requestAuctionStatistic = asyncTaskWrapper(async () => {
+    return await lazyStatistic;
+});
