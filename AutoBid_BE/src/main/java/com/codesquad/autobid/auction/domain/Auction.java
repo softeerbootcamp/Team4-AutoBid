732fcc284f065ce47bfd4cbe69e22d9f9a1633dc
WonjuLee <wonjulee.dev@gmail.com>
2023-02-11 20:38:51 +0900

    refacotr: 코드 포맷팅

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/domain/Auction.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/domain/Auction.java
@@ -4,6 +4,8 @@ import java.time.LocalDateTime;
 
 import javax.annotation.processing.Generated;
 
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import org.springframework.core.SpringVersion;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
@@ -23,46 +25,54 @@ import lombok.ToString;
 @Table("auction")
 public class Auction {
 
-	@Id
-	@Column(value = "auction_id")
-	private Long id;
-	@Column(value = "car_id")
-	private AggregateReference<Car, Long> carId;
-	@Column(value = "user_id")
-	private AggregateReference<User, Long> userId;
-	@Column(value = "auction_start_time")
-	private LocalDateTime auctionStartTime;
-	@Column(value = "auction_end_time")
-	private LocalDateTime auctionEndTime;
-	@Column(value = "auction_start_price")
-	private Long auctionStartPrice;
-	@Column(value = "auction_end_price")
-	private Long auctionEndPrice;
-	@Column(value = "auction_status")
-	private AuctionStatus auctionStatus;
-	@CreatedDate
-	@Column(value = "created_at")
-	private LocalDateTime createdAt;
-	@LastModifiedDate
-	@Column(value = "updated_at")
-	private LocalDateTime updatedAt;
+    @Id
+    @Column(value = "auction_id")
+    private Long id;
+    @Column(value = "car_id")
+    private AggregateReference<Car, Long> carId;
+    @Column(value = "user_id")
+    private AggregateReference<User, Long> userId;
+    @Column(value = "auction_start_time")
+    private LocalDateTime auctionStartTime;
+    @Column(value = "auction_end_time")
+    private LocalDateTime auctionEndTime;
+    @Column(value = "auction_start_price")
+    private Long auctionStartPrice;
+    @Column(value = "auction_end_price")
+    private Long auctionEndPrice;
+    @Column(value = "auction_status")
+    private AuctionStatus auctionStatus;
+    @CreatedDate
+    @Column(value = "created_at")
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    @Column(value = "updated_at")
+    private LocalDateTime updatedAt;
 
-	private Auction(Long carId, Long userId, LocalDateTime auctionStartTime, LocalDateTime auctionEndTime,
-		Long auctionStartPrice,
-		Long auctionEndPrice, AuctionStatus auctionStatus) {
-		this.carId = AggregateReference.to(carId);
-		this.userId = AggregateReference.to(userId);
-		this.auctionStartTime = auctionStartTime;
-		this.auctionEndTime = auctionEndTime;
-		this.auctionStartPrice = auctionStartPrice;
-		this.auctionEndPrice = auctionEndPrice;
-		this.auctionStatus = auctionStatus;
-	}
+    private Auction(Long carId, Long userId, LocalDateTime auctionStartTime, LocalDateTime auctionEndTime,
+                    Long auctionStartPrice,
+                    Long auctionEndPrice, AuctionStatus auctionStatus) {
+        this.carId = AggregateReference.to(carId);
+        this.userId = AggregateReference.to(userId);
+        this.auctionStartTime = auctionStartTime;
+        this.auctionEndTime = auctionEndTime;
+        this.auctionStartPrice = auctionStartPrice;
+        this.auctionEndPrice = auctionEndPrice;
+        this.auctionStatus = auctionStatus;
+    }
 
-	public static Auction of(Long carId, Long userId, LocalDateTime auctionStartTime, LocalDateTime auctionEndTime,
-		Long auctionStartPrice, Long auctionEndPrice, AuctionStatus auctionStatus) {
-		return new Auction(carId, userId, auctionStartTime, auctionEndTime, auctionStartPrice, auctionEndPrice,
-			auctionStatus);
-	}
+    public static Auction of(Long carId, Long userId, LocalDateTime auctionStartTime, LocalDateTime auctionEndTime,
+                             Long auctionStartPrice, Long auctionEndPrice, AuctionStatus auctionStatus) {
+        return new Auction(carId, userId, auctionStartTime, auctionEndTime, auctionStartPrice, auctionEndPrice,
+                auctionStatus);
+    }
+
+    public void openAuction() {
+        auctionStatus = AuctionStatus.PROGRESS;
+    }
+
+    public void finishAuction() {
+        auctionStatus = AuctionStatus.COMPLETED;
+    }
 
 }
