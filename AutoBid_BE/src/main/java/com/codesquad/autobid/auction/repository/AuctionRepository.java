aba124695cfb128bee8cf2e582cc2115559c496f
WonjuLee <wonjulee.dev@gmail.com>
2023-02-11 20:39:18 +0900

    feat: Auction 상태로 조회 기능 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/repository/AuctionRepository.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/repository/AuctionRepository.java
@@ -1,9 +1,14 @@
 package com.codesquad.autobid.auction.repository;
 
+import com.codesquad.autobid.auction.domain.AuctionStatus;
 import org.springframework.data.repository.CrudRepository;
 
 import com.codesquad.autobid.auction.domain.Auction;
 
+import java.util.List;
+
 public interface AuctionRepository extends CrudRepository<Auction, Long> {
 
+
+    List<Auction> getAuctionByAuctionStatus(AuctionStatus auctionStatus);
 }
