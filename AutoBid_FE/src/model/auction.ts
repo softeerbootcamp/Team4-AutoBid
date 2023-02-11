1360dfd59c31439baeb0a712f282f72f7da052ce
RandomlyChristen <yylwd2000@naver.com>
2023-02-12 07:26:52 +0900

    add: auction model에 startTime, endTime, startPrice, currPrice, endPrice 추가

--- AutoBid_FE/src/model/auction.ts
+++ AutoBid_FE/src/model/auction.ts
@@ -8,8 +8,13 @@ export enum AuctionStatus {
 }
 
 export type Auction = {
-    id: number, images: string[], tags: string[],
-    title: string, carInfo: CarInfo, price: number, status: AuctionStatus
+    id: number, images: string[],
+    title: string, carInfo: CarInfo,
+    startPrice: number,
+    currPrice: number,
+    endPrice: number,
+    status: AuctionStatus,
+    startTime: string, endTime: string
 };
 
 export type AuctionDTO = {
