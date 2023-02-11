7c05c7711a609e48fa026a86baf82ae53098d011
RandomlyChristen <yylwd2000@naver.com>
2023-02-12 03:52:48 +0900

    add: App이 QuerySidebar를 렌더링하도록 추가

--- AutoBid_FE/src/component/App.ts
+++ AutoBid_FE/src/component/App.ts
@@ -2,12 +2,14 @@ import Component from "../core/component";
 import Header from "./Header/Header";
 import AuctionList from "./AuctionList/AuctionList";
 import "./app.css";
+import QuerySidebar from "./QuerySidebar/QuerySidebar";
 
 class App extends Component<any> {
     template(): InnerHTML["innerHTML"] {
         return `
         <header data-component="Header"></header>
         <div class="main-container">
+            <div data-component="QuerySidebar"></div>
             <div data-component="AuctionList"></div>
         </div>
         `;
@@ -19,6 +21,9 @@ class App extends Component<any> {
 
         const $auctionList = document.querySelector('[data-component="AuctionList"]') as HTMLElement;
         new AuctionList($auctionList, {});
+
+        const $querySidebar = document.querySelector('[data-component="QuerySidebar"]') as HTMLElement;
+        new QuerySidebar($querySidebar, {});
     }
 }
 
