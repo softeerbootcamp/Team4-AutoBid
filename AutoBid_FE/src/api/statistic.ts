5fdb8b6aa216c822a6e35204c932c6cca0114585
RandomlyChristen <yylwd2000@naver.com>
2023-02-12 02:54:11 +0900

    test: lazyReturn을 매 요청마다 수행하도록 수정

--- AutoBid_FE/src/api/statistic.ts
+++ AutoBid_FE/src/api/statistic.ts
@@ -1,32 +1,30 @@
 import {AuctionStatisticDTO} from "../model/statistic";
 import {asyncTaskWrapper, lazyReturn} from "../core/util";
 
-const lazyStatistic = lazyReturn({
-    auctionStatistic: {
-        nSold: 114,
-        minPrice: 0,
-        maxPrice: 11200,
-        histogram: {
-            binSize: 100,
-            contents: [
-                10,
-                22,
-                24,
-                15,
-                35,
-                55,
-                67,
-                58,
-                43,
-                21,
-                13,
-                11,
-                9
-            ]
-        }
-    }
-} as AuctionStatisticDTO, 1000);
-
 export const requestAuctionStatistic = asyncTaskWrapper(async () => {
-    return await lazyStatistic;
+    return await lazyReturn({
+        auctionStatistic: {
+            nSold: 114,
+            minPrice: 0,
+            maxPrice: 11200,
+            histogram: {
+                binSize: 100,
+                contents: [
+                    10,
+                    22,
+                    24,
+                    15,
+                    35,
+                    55,
+                    67,
+                    58,
+                    43,
+                    21,
+                    13,
+                    11,
+                    9
+                ]
+            }
+        }
+    } as AuctionStatisticDTO, 1000);
 });
