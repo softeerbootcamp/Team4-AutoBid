a8da96b66a7f450eac2b55ff096aa0f5f872e1bb
WonjuLee <wonjulee.dev@gmail.com>
2023-02-12 17:01:32 +0900

    feat: redis transaction 설정

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/RedisConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/RedisConfig.java
@@ -1,6 +1,8 @@
 package com.codesquad.autobid.config;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -9,10 +11,16 @@ import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactor
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 300000000)
+@EnableTransactionManagement
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
@@ -27,7 +35,19 @@ public class RedisConfig {
         redisTemplate.setKeySerializer(new StringRedisSerializer());
         redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
         redisTemplate.setConnectionFactory(connectionFactory);
+        redisTemplate.setEnableTransactionSupport(true); // redis transaction
         return redisTemplate;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager() {
+        return new DataSourceTransactionManager(datasource());
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource datasource() {
+        return DataSourceBuilder.create().build();
     }
 
     @Bean
