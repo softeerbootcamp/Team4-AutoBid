b481a51f4c08284fe653557633ac573fe48d1141
WonjuLee <wonjulee.dev@gmail.com>
2023-02-12 17:06:05 +0900

    feat: 스케쥴러에서 경매 열기, 닫기 호출

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/scheduler/AuctionScheduler.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/scheduler/AuctionScheduler.java
@@ -1,7 +1,9 @@
 package com.codesquad.autobid.auction.scheduler;
 
+import com.codesquad.autobid.auction.service.AuctionService;
 import lombok.extern.slf4j.Slf4j;
 import net.javacrumbs.shedlock.spring.annotation.SchedulerLock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -12,19 +14,35 @@ import java.time.LocalDateTime;
 @Component
 public class AuctionScheduler {
 
-    private static final String CRON_RATE = "0 */15 * * * *";
+    // private static final String CRON_RATE = "0 */15 * * * *";
+    private static final String CRON_RATE = "*/10 * * * * *";
+
+
+    private final AuctionService auctionService;
+
+    @Autowired
+    public AuctionScheduler(AuctionService auctionService) {
+        this.auctionService = auctionService;
+    }
 
     @Async
     @Scheduled(cron = CRON_RATE)
     @SchedulerLock(name = "openPendingAuctionsLock")
     public void openPendingAuctions() {
-        log.info("openPendingAuctions: " + LocalDateTime.now());
+        log.info("openPendingAuctions");
+        auctionService.openPendingAuctions(getTime());
     }
 
     @Async
     @Scheduled(cron = CRON_RATE)
     @SchedulerLock(name = "closeInProgressAuctionsLock")
     public void closeInProgressAuctions() {
-        log.info("closeInProgressAuctions: " + LocalDateTime.now());
+        log.info("closeInProgressAuctions");
+        auctionService.closeFulfilledAuctions(getTime());
+    }
+
+    private LocalDateTime getTime() {
+        LocalDateTime now = LocalDateTime.now();
+        return LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), now.getHour(), now.getMinute());
     }
 }
