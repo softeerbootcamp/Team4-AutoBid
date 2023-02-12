7c9bf4502b5b2c648ca71ab1891952db469ba399
WonjuLee <wonjulee.dev@gmail.com>
2023-02-12 17:03:29 +0900

    refactor: redis에 올릴 데이터 변경

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/repository/AuctionRedisKey.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/repository/AuctionRedisKey.java
@@ -6,13 +6,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum AuctionRedisKey {
-
     PRICE("auction#%d_price"),
-    USERS("auction#%d_users"),
-    TOP_5_USERS("auction#%d_top_5_users"),
-    START_TIME("auction#%d_start_time"),
-    END_TIME("auction#%d_end_time"),
-    BINS("auction#%d_bins");
+    NUMBER_OF_USERS("auction#%d_number_of_users"),
+    BIDDERS("auction#%d_bidders");
 
     private String fieldName;
 }
