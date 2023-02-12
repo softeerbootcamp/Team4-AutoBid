c5569786727752db8166e55cdc96acf9245d456a
WonjuLee <wonjulee.dev@gmail.com>
2023-02-13 00:33:55 +0900

    refactor: change test target

---
+++ AutoBid_BE/src/test/java/com/codesquad/autobid/auction/repository/AuctionRepositoryTest.java
@@ -0,0 +1,10 @@
+package com.codesquad.autobid.auction.repository;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class AuctionRedisRepositoryTest {
+
+}
