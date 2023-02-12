6c17592386a872ae7d10bc07f82f4a033e4f79f0
WonjuLee <wonjulee.dev@gmail.com>
2023-02-13 00:36:23 +0900

    test: 경매상태, 시작 날짜로 경매 찾기 검증

--- AutoBid_BE/src/test/java/com/codesquad/autobid/auction/repository/AuctionRepositoryTest.java
+++ AutoBid_BE/src/test/java/com/codesquad/autobid/auction/repository/AuctionRepositoryTest.java
@@ -1,10 +1,55 @@
 package com.codesquad.autobid.auction.repository;
 
+import com.codesquad.autobid.auction.domain.Auction;
+import com.codesquad.autobid.auction.domain.AuctionStatus;
+import com.codesquad.autobid.car.domain.Car;
+import com.codesquad.autobid.car.repository.CarRepository;
+import com.codesquad.autobid.car.util.CarTestUtil;
+import com.codesquad.autobid.user.domain.User;
+import com.codesquad.autobid.user.repository.UserRepository;
+import com.codesquad.autobid.user.util.UserTestUtil;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
 @SpringBootTest
 @Transactional
-class AuctionRedisRepositoryTest {
+class AuctionRepositoryTest {
 
+    @Autowired
+    AuctionRepository auctionRepository;
+    @Autowired
+    UserRepository userRepository;
+    @Autowired
+    CarRepository carRepository;
+
+    @Test
+    @DisplayName("시작 날짜, 상태로 경매 찾기")
+    void findByStartDateAndStatus() {
+        // given
+        User user = UserTestUtil.getNewUser();
+        userRepository.save(user);
+        Car car = CarTestUtil.getNewCars(user.getId(), 1).get(0);
+        carRepository.save(car);
+
+        LocalDateTime now = LocalDateTime.now();
+        LocalDateTime startDate = LocalDateTime.of(now.getYear(), now.getMonth(), now.getDayOfMonth(), now.getHour(), now.getMinute(), now.getSecond());
+        Auction auction = Auction.of(car.getId(), user.getId(), startDate, LocalDateTime.now(), 1l, 2l, AuctionStatus.BEFORE);
+        auctionRepository.save(auction);
+        // when
+        List<Auction> findAuctions = auctionRepository.getAuctionByAuctionStatusAndAuctionStartTime(AuctionStatus.BEFORE, startDate);
+        // then
+        assertAll(
+                () -> assertThat(findAuctions.size()).isEqualTo(1),
+                () -> assertThat(findAuctions.get(0).getId()).isEqualTo(auction.getId())
+        );
+    }
 }
