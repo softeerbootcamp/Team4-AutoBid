f7d1f4117730bcf192e81f772d65e1461065bcf2
RandomlyChristen <yylwd2000@naver.com>
2023-02-13 21:47:23 +0900

    feat: #137 Header 에서 새로운 유저 모델을 적용, App 에서 이미 가진 세션으로 로그인 시도

--- AutoBid_FE/src/component/Header/Header.ts
+++ AutoBid_FE/src/component/Header/Header.ts
@@ -8,7 +8,7 @@ class Header extends Component<UserState> {
     }
 
     template(): InnerHTML["innerHTML"] {
-        const { isLogin, username } = this.state || { isLogin: false, username: "" };
+        const { isLogin, userName } = this.state || { isLogin: false, userName: "" };
         return `
         <div class="header__container">
             <h2 class="header__container__title">AutoBid</h2>
@@ -16,7 +16,7 @@ class Header extends Component<UserState> {
                 <button class="header__container__content header__container__content--btn my-page-btn">내 차 목록</button>
                 <button class="header__container__content header__container__content--btn my-page-btn">경매 목록</button>
                 ${isLogin ?
-                    `<span class="header__container__content header__container__content--username">${username}님 안녕하세요</span>
+                    `<span class="header__container__content header__container__content--username">${userName}님 안녕하세요</span>
                     <button class="header__container__content header__container__content--btn logout-btn">
                         <i class="fas fa-sign-out-alt fa-lg"></i>
                     </button>` 
