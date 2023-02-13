7c965c53fb54c5cd2cd5a4a2e429401e66d6f290
RandomlyChristen <yylwd2000@naver.com>
2023-02-13 20:46:24 +0900

    feat: #138 AnimatedNumber을 컴포넌트에 맞게 사용하는 코드 적용
    mount 시점에서 애니메이션이 발생하므로, animte 시점 === mount 시점.

--- AutoBid_FE/src/component/AuctionCard/AuctionCard.ts
+++ AutoBid_FE/src/component/AuctionCard/AuctionCard.ts
@@ -2,9 +2,9 @@ import Component from "../../core/component";
 import ImageSlider from "../ImageSlider/ImageSlider";
 import {CarInfo, getCarTypeName} from "../../model/car";
 import {Auction, AuctionStatus} from "../../model/auction";
-import "./auctioncard.css";
 import {deltaTimeToString} from "../../core/util";
 import AnimatedNumber from "../AnimatedNumber/AnimatedNumber";
+import "./auctioncard.css";
 
 
 const getInfoStr = ({ distance, type, sellName }: CarInfo) =>
@@ -13,7 +13,7 @@ const getInfoStr = ({ distance, type, sellName }: CarInfo) =>
 class AuctionCard extends Component<any, { auction: Auction, onClick: (arg: any) => any }> {
     template(): InnerHTML["innerHTML"] {
         const { title, carInfo, status } = this.props.auction;
-        const inner = `
+        return `
         ${status === AuctionStatus.PROGRESS ? `<div class="ribbon"><span>입찰 진행 중</span></div>` : ''}
         ${status === AuctionStatus.COMPLETE ? `<div class="sold-out"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M256 512c141.4 0 256-114.6 256-256S397.4 0 256 0S0 114.6 0 256S114.6 512 256 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z"/></svg></div>` : ''}
         <div class="card-item__img-slider" data-component="ImageSlider"></div>
@@ -24,7 +24,6 @@ class AuctionCard extends Component<any, { auction: Auction, onClick: (arg: any)
             <h4 class="timer">계산 중</h4>
         </div>
         `;
-        return inner;
     }
 
     mounted() {
@@ -33,6 +32,7 @@ class AuctionCard extends Component<any, { auction: Auction, onClick: (arg: any)
         new ImageSlider($imageSlider, { imageUrls: images, width: 250, height: 140 });
 
         this.timeoutRecursive();
+        this.animatePriceNumber();
     }
 
     timeoutRecursive() {
@@ -59,33 +59,38 @@ class AuctionCard extends Component<any, { auction: Auction, onClick: (arg: any)
     }
 
     priceInfo() {
-        const { status, startPrice, endPrice, currPrice } = this.props.auction;
+        const { status, startPrice, endPrice } = this.props.auction;
         switch (status) {
             case AuctionStatus.BEFORE:
                 return `
-                <h3 class="card-item__details__price status--before">
-                    <em>시작가</em><b class="animated-number" value="${currPrice}">0</b>만원
-                </h3>
+                <div class="card-item__details__price status--before">
+                    <em>시작가</em><b><div data-component="AnimatedNumber" data-price="${startPrice}"></div></b>만원
+                </div>
                 `;
             case AuctionStatus.COMPLETE:
                 return `
                 <h3 class="card-item__details__price status--complete">
-                    <em>낙찰가</em><b class="animated-number" value="${currPrice}">0</b>만원
+                    <em>낙찰가</em><b><div data-component="AnimatedNumber" data-price="${endPrice}"></div></b>만원
                 </h3>
                 `;
             case AuctionStatus.PROGRESS:
                 return `
                 <h3 class="card-item__details__price status--progress">
-                    <em>입찰가</em><b class="animated-number" value="${currPrice}">0</b>만원
+                    <em>입찰가</em><b><div data-component="AnimatedNumber" data-price="${endPrice}"></div></b>만원
                 </h3>
                 `;
         }
     }
 
+    animatePriceNumber() {
+        const $animatedNumber = this.$target.querySelector('[data-component="AnimatedNumber"]') as HTMLElement;
+        const destination = parseInt($animatedNumber.dataset.price as string);
+        new AnimatedNumber($animatedNumber, { start: 0, destination, speed: 300 });
+    }
+
     initialize() {
         const { onClick } = this.props;
         this.addEvent('click', '.card-item__details-container', onClick);
-        AnimatedNumber();
     }
 }
 
