3e7589dfe23abf6337ffebd437ec63882c1fa935
roopre <benny1020@naver.com>
2023-02-14 10:43:24 +0900

    feat: 필터 검색 서비스 로직 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/service/AuctionService.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/service/AuctionService.java
@@ -1,12 +1,24 @@
 package com.codesquad.autobid.auction.service;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.data.jdbc.core.mapping.AggregateReference;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.codesquad.autobid.auction.domain.Auction;
+import com.codesquad.autobid.auction.domain.AuctionInfoDto;
 import com.codesquad.autobid.auction.domain.AuctionStatus;
 import com.codesquad.autobid.auction.repository.AuctionRedis;
 import com.codesquad.autobid.auction.repository.AuctionRedisRepository;
 import com.codesquad.autobid.auction.repository.AuctionRepository;
 import com.codesquad.autobid.auction.repository.Bidder;
 import com.codesquad.autobid.auction.request.AuctionRegisterRequest;
+import com.codesquad.autobid.auction.response.AuctionInfoListResponse;
+import com.codesquad.autobid.car.repository.CarRepository;
 import com.codesquad.autobid.email.EmailService;
 import com.codesquad.autobid.image.domain.Image;
 import com.codesquad.autobid.image.repository.ImageRepository;
@@ -24,11 +36,17 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Set;
 
+import lombok.RequiredArgsConstructor;
+
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
 @Service
 public class AuctionService {
 
+	private final S3Uploader s3Uploader;
+	private final AuctionRepository auctionRepository;
+	private final ImageRepository imageRepository;
+	private final CarRepository carRepository;
     private final S3Uploader s3Uploader;
     private final AuctionRepository auctionRepository;
     private final AuctionRedisRepository auctionRedisRepository;
@@ -100,4 +118,49 @@ public class AuctionService {
         auctionRedisRepository.delete(auction);
         return auctionRedis.getBidders();
     }
+	public AuctionInfoListResponse getAuctions(String carType, String auctionStatus, Long startPrice, Long endPrice,
+		int page, int size) {
+		List<AuctionInfoDto> auctionInfoDtoList = getAuctionDtoList(carType, auctionStatus, startPrice, endPrice);
+		System.out.println(auctionInfoDtoList);
+		return getAuctionInfoListResponse(auctionInfoDtoList, page, size);
+	}
+
+	public List<AuctionInfoDto> getAuctionDtoList(String carType, String auctionStatus, Long startPrice,
+		Long endPrice) {
+		List<AuctionInfoDto> auctionInfoDtoList;
+
+		if (carType.equals("ALL") && auctionStatus.equals("ALL")) { // 둘 다 ALL인 경우
+			auctionInfoDtoList = auctionRepository.findAllByFilter(startPrice, endPrice);
+		} else if (carType.equals("ALL")) { // carType만 ALL인 경우
+			auctionInfoDtoList = auctionRepository.findAllByFilterWithAuctionStatus(startPrice, endPrice,
+				auctionStatus);
+		} else if (auctionStatus.equals("ALL")) { // auctionStatus만 ALL인 경우
+			auctionInfoDtoList = auctionRepository.findAllByFilterWithCarType(startPrice, endPrice, carType);
+		} else { // 둘 다 ALL 아닌 경우
+			auctionInfoDtoList = auctionRepository.findAllByFilterWithAuctionStatusAndCarType(startPrice, endPrice,
+				auctionStatus, carType);
+		}
+
+		return auctionInfoDtoList;
+	}
+
+	// 1 0~4
+	// 2 5 ~ 9 (size-1)*page ~ size*page -1
+	// 3 10 ~ 14
+	public AuctionInfoListResponse getAuctionInfoListResponse(List<AuctionInfoDto> auctionInfoDtoList, int page,
+		int size) {
+		int totalAuctionNum = auctionInfoDtoList.size();
+		if (totalAuctionNum < page * size - 1) {
+			auctionInfoDtoList.subList((size - 1) * page, totalAuctionNum - 1);
+		} else {
+			auctionInfoDtoList.subList((size - 1) * page, (size) * page - 1);
+		}
+		auctionInfoDtoList.forEach(auctionInfoDto -> {
+			List<Image> images = imageRepository.findAllByAuctionId(
+				AggregateReference.to(auctionInfoDto.getAuctionId()));
+			auctionInfoDto.setImages(images.stream().map(Image::getImageUrl).collect(Collectors.toList()));
+		});
+
+		return AuctionInfoListResponse.of(auctionInfoDtoList, totalAuctionNum);
+	}
 }
