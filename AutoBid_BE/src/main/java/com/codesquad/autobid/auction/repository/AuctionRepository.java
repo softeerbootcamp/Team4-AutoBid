958b1ba932a51fbcf7eddb7d526b7fe6847bbbbd
roopre <benny1020@naver.com>
2023-02-14 10:43:07 +0900

    feat: 필터 검색을 위한 join 쿼리 작성

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/repository/AuctionRepository.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/repository/AuctionRepository.java
@@ -1,15 +1,37 @@
 package com.codesquad.autobid.auction.repository;
 
-import com.codesquad.autobid.auction.domain.Auction;
-import com.codesquad.autobid.auction.domain.AuctionStatus;
-import org.springframework.data.repository.CrudRepository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
+import org.springframework.data.jdbc.repository.query.Query;
+import org.springframework.data.repository.CrudRepository;
+
+import com.codesquad.autobid.auction.domain.Auction;
+import com.codesquad.autobid.auction.domain.AuctionInfoDto;
+import com.codesquad.autobid.auction.domain.AuctionStatus;
+import java.time.LocalDateTime;
+import java.util.List;
 public interface AuctionRepository extends CrudRepository<Auction, Long> {
+	// default : start price, auction price
+	// auction status, car type
+	@Query(value = "select * from auction a join car c on a.car_id = c.car_id where a.auction_end_price >= :auctionStartPrice and a.auction_end_price <= :auctionEndPrice order by a.auction_start_time desc")
+	public List<AuctionInfoDto> findAllByFilter(Long auctionStartPrice, Long auctionEndPrice);
 
-    List<Auction> getAuctionByAuctionStatusAndAuctionStartTime(AuctionStatus auctionStatus, LocalDateTime startTime);
+	@Query("select * from auction a join car c on a.car_id = c.car_id where a.auction_end_price >= :auctionStartPrice and a.auction_end_price <= :auctionEndPrice and a.auction_status = :auctionStatus order by a.auction_start_time desc")
+	public List<AuctionInfoDto> findAllByFilterWithAuctionStatus(Long auctionStartPrice, Long auctionEndPrice,
+		String auctionStatus);
 
-    List<Auction> getAuctionByAuctionStatusAndAuctionEndTime(AuctionStatus auctionStatus, LocalDateTime endTime);
+	@Query("select * from auction a join car c on a.car_id = c.car_id where a.auction_end_price >= :auctionStartPrice and a.auction_end_price <= :auctionEndPrice and c.car_type = :carType order by a.auction_start_time desc")
+	public List<AuctionInfoDto> findAllByFilterWithCarType(Long auctionStartPrice, Long auctionEndPrice,
+		String carType);
+
+	@Query("select * from auction a join car c on a.car_id = c.car_id where a.auction_end_price >= :auctionStartPrice and a.auction_end_price <= :auctionEndPrice and a.auction_status = :auctionStatus and c.car_type = :carType order by a.auction_start_time desc")
+	public List<AuctionInfoDto> findAllByFilterWithAuctionStatusAndCarType(Long auctionStartPrice, Long auctionEndPrice,
+		String auctionStatus,
+		String carType);
+
+	List<Auction> getAuctionByAuctionStatusAndAuctionStartTime(AuctionStatus auctionStatus, LocalDateTime startTime);
+
+	List<Auction> getAuctionByAuctionStatusAndAuctionEndTime(AuctionStatus auctionStatus, LocalDateTime endTime);
+
 }
