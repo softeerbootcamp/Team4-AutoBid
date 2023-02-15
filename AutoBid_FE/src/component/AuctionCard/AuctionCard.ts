91174c4c2c50be3457a8263155de9867dd9c3d4e
Jaehyun Cho <33449712+hyunrice98@users.noreply.github.com>
2023-02-15 15:35:54 +0900

    feat: red ribbon for end-bed impelmented

--- AutoBid_FE/src/component/AuctionCard/AuctionCard.ts
+++ AutoBid_FE/src/component/AuctionCard/AuctionCard.ts
@@ -10,12 +10,15 @@ import "./auctioncard.css";
 const getInfoStr = ({ distance, type, sellName }: CarInfo) =>
     `${sellName} | ${distance.toLocaleString()}km | ${getCarTypeName(type)}`;
 
+
 class AuctionCard extends Component<any, { auction: Auction, onClick: (arg: any) => any }> {
     template(): InnerHTML["innerHTML"] {
         const { title, carInfo, status } = this.props.auction;
+        // Code for hammer (completed bid background)
+        // ${status === AuctionStatus.COMPLETED ? `<!--<div class="sold-out"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512">&lt;!&ndash;! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. &ndash;&gt;<path d="M318.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-120 120c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l4-4L325.4 293.4l-4 4c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l120-120c12.5-12.5 12.5-32.8 0-45.3l-16-16c-12.5-12.5-32.8-12.5-45.3 0l-4 4L330.6 74.6l4-4c12.5-12.5 12.5-32.8 0-45.3l-16-16zm-152 288c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l48 48c12.5 12.5 32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-1.4-1.4L272 285.3 226.7 240 168 298.7l-1.4-1.4z"/></svg></div>-->` : ''}
         return `
         ${status === AuctionStatus.PROGRESS ? `<div class="ribbon"><span>입찰 진행 중</span></div>` : ''}
-        ${status === AuctionStatus.COMPLETED ? `<div class="sold-out"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><!--! Font Awesome Pro 6.3.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M318.6 9.4c-12.5-12.5-32.8-12.5-45.3 0l-120 120c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l4-4L325.4 293.4l-4 4c-12.5 12.5-12.5 32.8 0 45.3l16 16c12.5 12.5 32.8 12.5 45.3 0l120-120c12.5-12.5 12.5-32.8 0-45.3l-16-16c-12.5-12.5-32.8-12.5-45.3 0l-4 4L330.6 74.6l4-4c12.5-12.5 12.5-32.8 0-45.3l-16-16zm-152 288c-12.5-12.5-32.8-12.5-45.3 0l-112 112c-12.5 12.5-12.5 32.8 0 45.3l48 48c12.5 12.5 32.8 12.5 45.3 0l112-112c12.5-12.5 12.5-32.8 0-45.3l-1.4-1.4L272 285.3 226.7 240 168 298.7l-1.4-1.4z"/></svg></div>` : ''}
+        ${status === AuctionStatus.COMPLETED ? `<div class="ribbon red-ribbon"><span>입찰 완료</span></div>` : ''}
         <div class="card-item__img-slider" data-component="ImageSlider"></div>
         <div class="card-item__details-container">
             <h4 class="card-item__details__title">${title}</h4>
