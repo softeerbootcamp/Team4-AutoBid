32fc36203ff150a644c1efcbccf9ccef0a2e82a7
Jaehyun Cho <33449712+hyunrice98@users.noreply.github.com>
2023-02-16 17:27:13 +0900

    feat: Mypage component template implemented

--- AutoBid_FE/src/component/MyPage/MyPage.ts
+++ AutoBid_FE/src/component/MyPage/MyPage.ts
@@ -1,6 +1,7 @@
 import Component from "../../core/component";
 import {requestParticipationList, requestMyList, requestUserCarList} from "../../api/mypage";
 import {Auction} from "../../model/auction";
+import AuctionList from "../AuctionList/AuctionList";
 
 class MyPage extends Component<any> {
     private participationList: Auction[] = [];
@@ -51,7 +52,48 @@ class MyPage extends Component<any> {
 
     template(): InnerHTML["innerHTML"] {
         return `
-            
+    <div class="my-page__section-holder my-page__main__my-car-list__holder">
+        <h3>참여 경매</h3>
+        <hr>
+        <div class="my-page__card-slider my-page__main__my-car-list">
+            <button class="my-page__swap-button-left"><i class="fas fa-chevron-left"></i></button>
+                ${this.setCards(this.participationList)}
+            <button class="my-page__swap-button-right"><i class="fas fa-chevron-right"></i></button>
+        </div>
+    </div>
+    <div class="my-page__section-holder my-page__main__participating-bids__holder">
+        <h3>등록 경매</h3>
+        <hr>
+        <div class="my-page__card-slider my-page__main__participating-bids">
+            <button class="my-page__swap-button-left"><i class="fas fa-chevron-left"></i></button>
+                ${this.setCards(this.myCarList)}
+            <button class="my-page__swap-button-right"><i class="fas fa-chevron-right"></i></button>
+        </div>
+    </div>
+    <div class="my-page__section-holder my-page__main__registered-bids__holder">
+        <h3>내 차 목록</h3>
+        <hr>
+        <div class="my-page__card-slider my-page__main__registered-bids">
+            <button class="my-page__swap-button-left"><i class="fas fa-chevron-left"></i></button>
+                ${this.setCards(this.userCarList)}
+            <button class="my-page__swap-button-right"><i class="fas fa-chevron-right"></i></button>
+        </div>
+    </div>
+        `
+    }
+
+    setCards(auctionList: Auction[]): InnerHTML['innerHTML'] {
+        if (auctionList && auctionList.length) {
+            return `
+                <div class="my-page__card-list__holder">
+                    ${auctionList.map(() => {
+                        `<div data-component="AuctionCard"></div>`
+                    }).join('')}
+                </div>
+            `
+        }
+        return `
+            <div class="no-content">조회된 경매 없음</div>
         `
     }
 
