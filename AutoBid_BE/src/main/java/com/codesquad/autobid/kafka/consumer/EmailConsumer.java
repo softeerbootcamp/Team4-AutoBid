ecaadb12d051d1c99c7fd6bc75669992f58cde39
WonjuLee <wonjulee.dev@gmail.com>
2023-02-16 12:04:30 +0900

    feat: Email 보내기 consumer 구현

--- AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/consumer/EmailConsumer.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/consumer/EmailConsumer.java
@@ -1,15 +1,22 @@
 package com.codesquad.autobid.kafka.consumer;
 
+import com.codesquad.autobid.auction.domain.Auction;
+import com.codesquad.autobid.email.EmailService;
+import com.codesquad.autobid.user.domain.User;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class EmailConsumer {
 
-    @KafkaListener(topics = {"EMAIL_TOPIC"})
-    public void consumeMessage(String message) {
-        log.debug("consumeMessage: {} ", message);
+    private final EmailService emailService;
+
+    @KafkaListener(topics = "auction-email")
+    public void consumeMessage(Auction auction) {
+        emailService.send(auction, new User(), 1000l);
     }
 }
