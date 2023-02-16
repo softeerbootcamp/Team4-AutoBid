8063a70a757f044dfd930d96d07a4a53e653f69c
WonjuLee <wonjulee.dev@gmail.com>
2023-02-16 12:06:01 +0900

    feat: 경매 닫기 producer 구현

---
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/producer/AuctionCloseProducer.java
@@ -0,0 +1,25 @@
+package com.codesquad.autobid.kafka.producer;
+
+import com.codesquad.autobid.auction.domain.Auction;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class AuctionCloseProducer {
+
+    @Value("${spring.kafak.topic.auction-close}")
+    private String AUCTION_CLOSE_TOPIC_NAME;
+    private final KafkaTemplate kafkaTemplate;
+
+    public void produce(List<Auction> auctions) {
+        // todo: check serialize
+        for (Auction auction : auctions) {
+            kafkaTemplate.send(AUCTION_CLOSE_TOPIC_NAME, auction);
+        }
+    }
+}
