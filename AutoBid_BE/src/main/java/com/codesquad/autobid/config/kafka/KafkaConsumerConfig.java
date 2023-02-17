75bbdbe03b9536c6826662ecb587213b001feb20
roopre <benny1020@naver.com>
2023-02-17 10:23:34 +0900

    fix: 카프카 설정 수정

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/kafka/KafkaConsumerConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/kafka/KafkaConsumerConfig.java
@@ -4,6 +4,7 @@ import com.codesquad.autobid.config.kafka.deserializer.AuctionKafkaDTODeserializ
 import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaDTO;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,6 +18,9 @@ import java.util.Map;
 @EnableKafka
 public class KafkaConsumerConfig {
 
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String BOOTSTRAP_SERVERS;
+
     @Bean
     public ConsumerFactory<String, AuctionKafkaDTO> consumerFactory() {
         return new DefaultKafkaConsumerFactory<>(consumerProps());
@@ -24,7 +28,7 @@ public class KafkaConsumerConfig {
 
     private Map<String, Object> consumerProps() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, AuctionKafkaDTODeserializer.class);
