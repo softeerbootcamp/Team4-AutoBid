028987d29a860f91fb5e7f26d868df6351655f8e
RandomlyChristen <yylwd2000@naver.com>
2023-02-17 09:01:22 +0900

    feat: #188 모달 상태에 error 추가 및 Modal 컴포넌트에 ErrorView 렌더링 추가

--- AutoBid_FE/src/store/modal.ts
+++ AutoBid_FE/src/store/modal.ts
@@ -5,21 +5,23 @@ import {Reducer} from "redux";
 export enum ModalView {
     NONE,
     POSTING,
-    SHOWING
+    SHOWING,
+    ERROR
 }
 
 export enum ModalActionType {
     CLOSE='modal/CLOSE',
     POP_POSTING='modal/POP_POSTING',
-    POP_SHOWING='modal/POP_SHOWING'
+    POP_SHOWING='modal/POP_SHOWING',
+    POP_ERROR='modal/POP_ERROR'
 }
 
 export type ModalState = {
-    pop: boolean, view: ModalView, auction: Auction|undefined
+    pop: boolean, view: ModalView, auction: Auction|undefined, error: Error|undefined
 };
 
 export const MODAL_INITIAL: ModalState = {
-    pop: false, view: ModalView.POSTING, auction: undefined
+    pop: false, view: ModalView.NONE, auction: undefined, error: undefined
 };
 
 export const popShowingAuctionModal = (auction: Auction) => {
@@ -30,6 +32,10 @@ export const popPostingAuctionModal = () => {
     GlobalStore.get().dispatch({ type: ModalActionType.POP_POSTING });
 };
 
+export const popErrorModal = (error: Error) => {
+    GlobalStore.get().dispatch({ type: ModalActionType.POP_ERROR, error });
+}
+
 export const closeModal = () => {
     GlobalStore.get().dispatch({ type: ModalActionType.CLOSE });
 };
@@ -38,22 +44,25 @@ const modal: Reducer<ModalState> = (state = MODAL_INITIAL, action) => {
     switch (action.type) {
         case ModalActionType.CLOSE:
             return { ...state,
-                pop: false,
-                view: ModalView.NONE,
-                auction: undefined
+                pop: false
             };
         case ModalActionType.POP_SHOWING:
-            return { ...state,
+            return { ...MODAL_INITIAL,
                 pop: true,
                 view: ModalView.SHOWING,
                 auction: action.auction,
             };
         case ModalActionType.POP_POSTING:
-            return { ...state,
+            return { ...MODAL_INITIAL,
                 pop: true,
-                view: ModalView.POSTING,
-                auction: undefined
+                view: ModalView.POSTING
             };
+        case ModalActionType.POP_ERROR:
+            return { ...MODAL_INITIAL,
+                pop: true,
+                view: ModalView.ERROR,
+                error: action.error
+            }
         default:
             return state;
     }
