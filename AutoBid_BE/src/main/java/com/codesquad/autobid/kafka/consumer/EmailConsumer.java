4b7faa74264d24e78f6cd04e3041978fa8674b52
WonjuLee <wonjulee.dev@gmail.com>
2023-02-20 12:34:45 +0900

    feat: implement email consumer

--- AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/consumer/EmailConsumer.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/consumer/EmailConsumer.java
@@ -1,12 +1,15 @@
 package com.codesquad.autobid.kafka.consumer;
 
-import com.codesquad.autobid.auction.domain.Auction;
 import com.codesquad.autobid.email.EmailService;
-import com.codesquad.autobid.user.domain.User;
+import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaDTO;
+import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaUserDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @Slf4j
@@ -16,7 +19,12 @@ public class EmailConsumer {
     private final EmailService emailService;
 
     @KafkaListener(topics = "auction-email", groupId = "auction-email-consumer")
-    public void consumeMessage(Auction auction) {
-        emailService.send(auction, new User(), 1000l);
+    public void consume(@Payload AuctionKafkaDTO auctionKafkaDTO) {
+        List<AuctionKafkaUserDTO> users = auctionKafkaDTO.getUsers();
+        for (AuctionKafkaUserDTO user : users) {
+            String auctionTitle = auctionKafkaDTO.getAuctionTitle();
+            Long endPrice = auctionKafkaDTO.getPrice();
+            emailService.send(auctionTitle, endPrice, user);
+        }
     }
 }
