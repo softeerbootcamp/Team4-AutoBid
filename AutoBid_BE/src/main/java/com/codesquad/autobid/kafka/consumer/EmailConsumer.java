3f8a9830726edf69502ba4ca6bed12b2f3b67f08
WonjuLee <wonjulee.dev@gmail.com>
2023-02-20 17:01:41 +0900

    refactor: objectMapper 적용

--- AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/consumer/EmailConsumer.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/consumer/EmailConsumer.java
@@ -3,10 +3,11 @@ package com.codesquad.autobid.kafka.consumer;
 import com.codesquad.autobid.email.EmailService;
 import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaDTO;
 import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaUserDTO;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -17,13 +18,17 @@ import java.util.List;
 public class EmailConsumer {
 
     private final EmailService emailService;
+    private final ObjectMapper om;
 
     @KafkaListener(topics = "auction-email", groupId = "auction-email-consumer")
-    public void consume(@Payload AuctionKafkaDTO auctionKafkaDTO) {
+    public void consume(String json) throws JsonProcessingException {
+        AuctionKafkaDTO auctionKafkaDTO = om.readValue(json, AuctionKafkaDTO.class);
         List<AuctionKafkaUserDTO> users = auctionKafkaDTO.getUsers();
+
         for (AuctionKafkaUserDTO user : users) {
             String auctionTitle = auctionKafkaDTO.getAuctionTitle();
             Long endPrice = auctionKafkaDTO.getPrice();
+
             emailService.send(auctionTitle, endPrice, user);
         }
     }
