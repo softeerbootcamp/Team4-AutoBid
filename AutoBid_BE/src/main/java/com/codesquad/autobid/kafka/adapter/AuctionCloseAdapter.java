ef6f4877f15be1c06efeec40954ceada5071a381
WonjuLee <wonjulee.dev@gmail.com>
2023-02-20 12:37:38 +0900

    refactor: consumer, producer

--- AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/adapter/AuctionCloseAdapter.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/kafka/adapter/AuctionCloseAdapter.java
@@ -1,18 +1,28 @@
 package com.codesquad.autobid.kafka.adapter;
 
 import com.codesquad.autobid.auction.domain.Auction;
-import com.codesquad.autobid.auction.repository.AuctionRedis;
+import com.codesquad.autobid.auction.repository.AuctionRedisBidderDTO;
+import com.codesquad.autobid.auction.repository.AuctionRedisDTO;
 import com.codesquad.autobid.auction.repository.AuctionRedisRepository;
 import com.codesquad.autobid.auction.repository.AuctionRepository;
+import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaDTO;
+import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaUserDTO;
+import com.codesquad.autobid.user.domain.User;
+import com.codesquad.autobid.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
-public class AuctionDataCloseAdapter {
+public class AuctionCloseAdapter {
 
     @Value("${spring.kafka.topic.auction-email}")
     private String AUCTION_EMAIL_TOPIC_NAME;
@@ -22,33 +32,37 @@ public class AuctionDataCloseAdapter {
     private final KafkaTemplate kafkaTemplate;
     private final AuctionRedisRepository auctionRedisRepository;
     private final AuctionRepository auctionRepository;
+    private final UserRepository userRepository;
 
     @KafkaListener(topics = "auction-close", groupId = "auction-close-consumer")
-    public void consume(Auction auction) {
+    public void consume(@Payload AuctionKafkaDTO auctionKafkaDTO) {
         // todo : auctionRedis 정리가 구체적으로 어떤 작업인가?
-        AuctionRedis auctionRedis = auctionRedisRepository.findById(auction.getId());
-        /*
-            {
-            	price: long,
-            	users: [ // 5명
-            		{
-            			userId: long,
-            			username: string,
-            			phoneNumber: string,
-            			price: long
-            		},
-            	],
-            	numberOfUsers: long
-            }
-         */
+        // redis
+        Auction auction = auctionKafkaDTO.getAuction();
+        AuctionRedisDTO auctionRedisDto = auctionRedisRepository.findById(auction.getId());
+        List<AuctionKafkaUserDTO> auctionKafkaUserDTOs = findBidders(auctionRedisDto.getAuctionRedisBidderDto());
+        auctionRedisRepository.delete(auction);
+
         // mysql
-        auction.close();
+        auction.markToFinish(auctionRedisDto.getPrice());
         auctionRepository.save(auction);
-        produce(auction);
+
+        auctionKafkaDTO.update(auctionKafkaUserDTOs);
+        produce(auctionKafkaDTO);
     }
 
-    public void produce(Auction auction) {
-        kafkaTemplate.send(AUCTION_EMAIL_TOPIC_NAME, auction);
-        kafkaTemplate.send(AUCTION_SEND_TOPIC_NAME, auction);
+    private List<AuctionKafkaUserDTO> findBidders(Set<AuctionRedisBidderDTO> auctionRedisBidderDtos) {
+        return auctionRedisBidderDtos.stream()
+            .map(auctionBidderDTO -> {
+                User user = userRepository.findById(auctionBidderDTO.getUserId()).get();
+                return AuctionKafkaUserDTO.from(auctionBidderDTO, user);
+            })
+            .collect(Collectors.toList());
+    }
+
+    public void produce(AuctionKafkaDTO auctionKafkaDto) {
+        // todo: check serializer
+        kafkaTemplate.send(AUCTION_EMAIL_TOPIC_NAME, auctionKafkaDto);
+        kafkaTemplate.send(AUCTION_SEND_TOPIC_NAME, auctionKafkaDto);
     }
 }
