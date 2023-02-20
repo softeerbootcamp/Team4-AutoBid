578273f202af1b22c3e1dd1835d3517cfd58f3a9
tjdwns4537 <tjdwns4537@naver.com>
2023-02-20 11:14:46 +0900

    fix: websocket 설정 수정

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
@@ -18,6 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
                 .setHandshakeHandler(new CustomHandshakeHandler())
+                .setAllowedOrigins("https://www.autobid.im")
                 .withSockJS();
     }
 
