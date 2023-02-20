60b39e33dfc4b7e3c9da23a342873fb97fbd732c
tjdwns4537 <tjdwns4537@naver.com>
2023-02-20 10:19:17 +0900

    fix: setOrigins nginx 에서 수행

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
@@ -17,7 +17,6 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/auction-room")
-                .setAllowedOriginPatterns("https://www.autobid.im/")
                 .setHandshakeHandler(new CustomHandshakeHandler())
                 .withSockJS();
     }
