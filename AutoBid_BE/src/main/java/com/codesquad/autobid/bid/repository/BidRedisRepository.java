45ec6553f5d707d18e2b1627bf58c5586afa3db2
tjdwns4537 <tjdwns4537@naver.com>
2023-02-20 18:19:25 +0900

    fix: update price to auction redis

--- AutoBid_BE/src/main/java/com/codesquad/autobid/bid/repository/BidRedisRepository.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/bid/repository/BidRedisRepository.java
@@ -5,6 +5,7 @@ import com.codesquad.autobid.auction.repository.AuctionRedisUtil;
 import com.codesquad.autobid.bid.domain.Bid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.data.redis.core.ZSetOperations;
 import org.springframework.stereotype.Repository;
 
@@ -14,17 +15,20 @@ import java.util.Map;
 public class BidRedisRepository {
 
     private final RedisTemplate redisTemplate;
-    private static ZSetOperations zSetOps;
+    private static  ZSetOperations zSetOps;
+    private static ValueOperations stringOps;
 
     @Autowired
     public BidRedisRepository(RedisTemplate redisTemplate) {
         this.redisTemplate = redisTemplate;
         zSetOps = redisTemplate.opsForZSet();
+        stringOps = redisTemplate.opsForValue();
     }
 
     public void save(Bid bid) {
         Long auctionId = bid.getAuctionId().getId();
         Map<AuctionRedisKey, String> keys = AuctionRedisUtil.generateKeys(auctionId);
         zSetOps.add(keys.get(AuctionRedisKey.BIDDERS), bid.getUserId().getId(), bid.getPrice());
+        stringOps.set(keys.get(AuctionRedisKey.PRICE), bid.getPrice());
     }
 }
