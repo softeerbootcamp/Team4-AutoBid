db7c1aa1f01d5bbc85e8d2ce37c13bc62c43bbc1
roopre <benny1020@naver.com>
2023-02-20 11:58:46 +0900

    Update WebSocketConfig.java

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/WebSocketConfig.java
@@ -17,8 +17,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws")
-                .setHandshakeHandler(new CustomHandshakeHandler())
-                .withSockJS();
+                registry.addEndpoint("/auction-room").setHandshakeHandler(new CustomHandshakeHandler()).setAllowedOrigins("https://www.autobid.im").withSockJS();
+
     }
 
     @Override
