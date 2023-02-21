50fcf08273498fce38d99ed53045dec90eee2197
tjdwns4537 <tjdwns4537@naver.com>
2023-02-21 11:18:20 +0900

    feat: implement factory method

--- AutoBid_BE/src/main/java/com/codesquad/autobid/websocket/domain/BidderDto.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/websocket/domain/BidderDto.java
@@ -1,12 +1,12 @@
 package com.codesquad.autobid.websocket.domain;
 
-import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaDTO;
 import com.codesquad.autobid.kafka.producer.dto.AuctionKafkaUserDTO;
+import lombok.AccessLevel;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class BidderDto {
     private Long userId;
     private String username;
@@ -14,16 +14,20 @@ public class BidderDto {
     private Long price;
 
     public static BidderDto from(AuctionKafkaUserDTO auctionKafkaUserDTO) {
-        return new BidderDto(auctionKafkaUserDTO.getId(), auctionKafkaUserDTO.getName(), auctionKafkaUserDTO.getPhoneNumber(), auctionKafkaUserDTO.getPrice());
+        BidderDto bidderDto = new BidderDto();
+        bidderDto.userId = auctionKafkaUserDTO.getId();
+        bidderDto.username = auctionKafkaUserDTO.getName();
+        bidderDto.phoneNumber = auctionKafkaUserDTO.getPhoneNumber();
+        bidderDto.price = auctionKafkaUserDTO.getPrice();
+        return bidderDto;
     }
 
-    public BidderDto(Long userId, String userName, String phoneNumber, Long price) {
-        this.userId = userId;
-        this.username = userName;
-        this.phoneNumber = phoneNumber;
-        this.price = price;
-    }
-
-    public BidderDto() {
+    public static BidderDto of(Long userId, Long price, String name, String mobilenum) {
+        BidderDto bidderDto = new BidderDto();
+        bidderDto.userId = userId;
+        bidderDto.username = name;
+        bidderDto.phoneNumber = mobilenum;
+        bidderDto.price = price;
+        return bidderDto;
     }
 }
