27527fc2c1e588429e4d348d8a2348314dd20c9c
roopre <benny1020@naver.com>
2023-02-21 11:56:08 +0900

    feat: cachemanager 설정

--- AutoBid_BE/src/main/java/com/codesquad/autobid/config/RedisConfig.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/config/RedisConfig.java
@@ -1,13 +1,23 @@
 package com.codesquad.autobid.config;
 
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.CacheKeyPrefix;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
@@ -15,6 +25,7 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @Configuration
 @EnableRedisHttpSession(maxInactiveIntervalInSeconds = 300000000)
 @EnableTransactionManagement
+@EnableCaching
 public class RedisConfig {
 
     @Value("${spring.redis.host}")
@@ -43,5 +54,29 @@ public class RedisConfig {
         configuration.setPort(port);
         configuration.setPassword(password);
         return new LettuceConnectionFactory(configuration);
+    }
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration configuration = RedisCacheConfiguration.defaultCacheConfig()
+            .disableCachingNullValues()
+            .entryTtl(Duration.ofSeconds(CacheKey.DEFAULT_EXPIRE_SEC))
+            .computePrefixWith(CacheKeyPrefix.simple())
+            .serializeKeysWith(
+                RedisSerializationContext.SerializationPair
+                    .fromSerializer(new StringRedisSerializer()))
+            .serializeValuesWith(
+                RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+
+        // 캐시 키 별 default 유효 시간 설정
+        Map<String, RedisCacheConfiguration> cacheConfiguration = new HashMap<>();
+        cacheConfiguration.put(CacheKey.ZONE, RedisCacheConfiguration.defaultCacheConfig()
+            .entryTtl(Duration.ofSeconds(CacheKey.ZONE_EXPIRE_SEC)));
+
+        return RedisCacheManager.RedisCacheManagerBuilder
+            .fromConnectionFactory(redisConnectionFactory)
+            .cacheDefaults(configuration)
+            .withInitialCacheConfigurations(cacheConfiguration)
+            .build();
     }
 }
