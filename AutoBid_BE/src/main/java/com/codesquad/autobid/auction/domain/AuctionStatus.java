f5c5a8405b87865a36134fde5cfa0d8a6823cad3
WonjuLee <wonjulee.dev@gmail.com>
2023-02-22 12:17:00 +0900

    refactor: remove unused static variable

--- AutoBid_BE/src/main/java/com/codesquad/autobid/auction/domain/AuctionStatus.java
+++ AutoBid_BE/src/main/java/com/codesquad/autobid/auction/domain/AuctionStatus.java
@@ -2,14 +2,12 @@ package com.codesquad.autobid.auction.domain;
 
 public enum AuctionStatus {
 
-	PROGRESS(0),
-	BEFORE(1),
-	COMPLETED(2);
-	private int status;
+    PROGRESS(0),
+    BEFORE(1),
+    COMPLETED(2);
+    private int status;
 
-	public static final Long BEFORE_END_PRICE = -1L;
-
-	AuctionStatus(int status) {
-		this.status = status;
-	}
+    AuctionStatus(int status) {
+        this.status = status;
+    }
 }
